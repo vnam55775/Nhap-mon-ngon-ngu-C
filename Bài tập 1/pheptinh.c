ca0b7ce9e94c3ddbbed6b00e05eeb4a4bfe71965
vnam55775 <75051204+vnam55775@users.noreply.github.com>
2020-11-26 08:41:50 +0700

    Create pheptinh.c

---
+++ Bài tập 1/pheptinh.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void) {
+int a = 3;
+int b = 5;
+printf("a + b = %d, ", a + b);
+printf("a - b = %d, ", a - b);
+printf("a * b = %d, ", a * b);
+printf("a / b = %d\n", a / b);
+double c = 3;
+double d = 5;
+printf("a / b = %f", c / d);
+  return 0; 
+}
