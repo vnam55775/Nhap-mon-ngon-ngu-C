179936f2f869a12fd150ebab33eb3b81e2d702d1
vnam55775 <75051204+vnam55775@users.noreply.github.com>
2020-12-23 18:07:09 -0800

    Create xinchao.c

---
+++ Bài tập 3/xinchao.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void) {
+
+  int count ;
+      printf ("\t xin chao \n ");
+  for ( count =1 ; count <= 10 ; count ++ )
+      printf (" \n \t \t xin chao \n");
+      
+  return 0;
+}
