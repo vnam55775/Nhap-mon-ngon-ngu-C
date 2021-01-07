667bec7577ff643f40e0c37620f4a6ce03c52a9f
vnam55775 <75051204+vnam55775@users.noreply.github.com>
2021-01-07 11:06:03 +0700

    Update kt_01.c

--- Bài tập 3/kt_01.c
+++ Bài tập 3/kt_01.c
@@ -1,21 +1,35 @@
 #include <stdio.h>
 
 int main()
+int main() 
 {
   //Khai bao bien i va j
-int i, j;
-  printf("\t                      BANG CUU CHUONG\n");
-  //Vong lap tu 1 den 10
-for (i = 1; i < 11; i++)
-{
-  //Vong lap nhan tu 1 den 10
-for (j = 1; j < 10; j++)
-{
-  //Hien thi man hinh ket qua bang cuu chuong ra man hinh
-  printf("%d*%d=%d\t", j, i, j * i);
-}
-  printf("\n");
-}
+	int tich1 = 2, tich2 =3, tich3 = 4;
+		printf("\tBANG CUU CHUONG\n");
+    	printf("2x1=%d\t\n",tich1 * 1);
+    	printf("2x2=%d\t\n",tich1 * 2);
+    	printf("2x3=%d\t\n",tich1 * 3 );
+    	printf("2x4=%d\t\n",tich1 * 4);
+    	printf("\nBANG CUU CHUONG\n");
+    	printf("3x1=%d\t\n",tich2 * 1);
+    	printf("3x2=%d\t\n",tich2 * 2);
+    	printf("3x3=%d\t\n",tich2 * 3);
+    	printf("3x4=%d\t\n",tich2 * 4);
+    	printf("\nBANG CUU CHUONG\n");
+    	printf("4x1=%d\t\n",tich3 * 1);
+   	printf("4x2=%d\t\n",tich3 * 2);
+   	printf("4x3=%d\t\n",tich3 * 3);
+   	printf("4x4=%d\t\n",tich3 * 4);
 
 return 0;
+  int a, b, c;
+  a = 2; b = 3; c = 4;
+  printf("\nBang 2\n");
+  for(int i = 1; i < 11; i++)
+  printf("\n%dx%d=%d\t",a ,i ,a * i);
+   printf("\nBang 3\n");
+  for(int i = 1; i < 11; i++)
+  printf("\n%dx%d=%d\t",b ,i ,b * i);
+   printf("\nBang 4\n");
+  return 0;
 }
